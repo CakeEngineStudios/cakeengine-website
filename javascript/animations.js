ff120ac8c62d7ebd8071b9a7130a689a8f17546e
CakeEngineStudios <168625675+CakeEngineStudios@users.noreply.github.com>
2024-08-02 21:54:11 +1200

    new try

---
+++ javascript/animations.js
@@ -0,0 +1,11 @@
+function do_top_bar_animation(){
+    var element = document.getElementById("home button");
+    element.style.animation = "index_slide 1s";
+
+    var element = document.getElementById("calamity button");
+    element.style.animation = "calamity_slide 1s";
+
+    var element = document.getElementById("docs button");
+    element.style.animation = "docs_slide 1s";
+
+}
---
+++ javascript/download_framwork.js
@@ -0,0 +1,16 @@
+//https://github.com/CakeEngineStudios/CakeGameEngine/archive/refs/heads/main.zip
+/*This code is fair use as it just opens a download link lol.*/
+
+async function Download_Framework(){
+    var cloneURL = 'https://github.com/CakeEngineStudios/CakeGameEngine/archive/refs/heads/main.zip';
+    var link = document.createElement('a');
+    link.href = cloneURL;
+    link.click()
+}
+
+async function Download_API(){
+    var cloneURL = 'https://github.com/CakeEngineStudios/CakeGameEngine/archive/refs/heads/main.zip';
+    var link = document.createElement('a');
+    link.href = cloneURL;
+    link.click()
+}
