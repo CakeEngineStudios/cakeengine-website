ace354522fb7539557a9e6e0ee1f018715dd2b63
CakeEngineStudios <168625675+CakeEngineStudios@users.noreply.github.com>
2024-08-02 19:50:50 +1200

    added website
    ds ad as dsa

---
+++ Website/javascript/change_pages.js
@@ -0,0 +1,11 @@
+function go_calamity() {
+    window.location.href = './calamity.html'; 
+}
+
+function go_docs() {
+    window.location.href = './docs.html'; 
+}
+
+function go_home() {
+    window.location.href = './index.html'; 
+}
